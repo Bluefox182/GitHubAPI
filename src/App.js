4d21f1f848b848e9920a111784e23c6a111da382
bluefox182 <fergaldoss@gmail.com>
2021-06-18 11:35:25 -0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -12,22 +12,28 @@ function App() {
   const [userInput, setUserInput] = useState("");
   const [error, setError] = useState(null);
 
+  useEffect(() => {
+    fetch("https://api.github.com/users/example")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="navbar">
+      <div className="search">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Input
+              placeholder="GitHub User"
+              name="github user"
+              onChange={handleSearch}
+            />
+            <Form.Button content="Search" />
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   );
 }

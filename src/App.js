1b7581b2476b35b92ccd60d00e18e5f0e4ed1140
bluefox182 <fergaldoss@gmail.com>
2021-06-18 11:37:52 -0500

    card data imported from semantic web

--- src/App.js
+++ src/App.js
@@ -34,6 +34,34 @@ function App() {
           </Form.Group>
         </Form>
       </div>
+
+      <div className="card">
+        <Card>
+          <Image src={avatar} wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>{name}</Card.Header>
+            <Card.Header>{userName}</Card.Header>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="user" />
+              {followers} Followers
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="user" />
+              {repos} Repos
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="user" />
+              {following} Following
+            </a>
+          </Card.Content>
+        </Card>
+      </div>
     </div>
   );
 }

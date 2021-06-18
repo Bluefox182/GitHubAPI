e2c9e71e794663326e749c74f9ac14b2d7859ea4
bluefox182 <fergaldoss@gmail.com>
2021-06-18 11:31:43 -0500

    variables declared

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Form, Card, Icon, Image } from "semantic-ui-react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [userName, setUsername] = useState("");
+  const [followers, setFollowers] = useState("");
+  const [following, setFollowing] = useState("");
+  const [repos, setRepos] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [error, setError] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">

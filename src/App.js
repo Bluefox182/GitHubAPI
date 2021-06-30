d3f360cf4b7b31d8d7339cb80c3f5c6da2627fff
bluefox182 <fergaldoss@gmail.com>
2021-06-30 15:14:58 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
   };
 
   const handleSubmit = () => {
-    fetch(`https>//api.github.com/users/${userInput}`)
+    fetch(`https://api.github.com/users/${userInput}`)
       .then((res) => res.json())
       .then((data) => {
         if (data.message) {
@@ -68,34 +68,37 @@ function App() {
           </Form.Group>
         </Form>
       </div>
-
-      <div className="card">
-        <Card>
-          <Image src={avatar} wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>{name}</Card.Header>
-            <Card.Header>{userName}</Card.Header>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name="user" />
-              {followers} Followers
-            </a>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name="user" />
-              {repos} Repos
-            </a>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name="user" />
-              {following} Following
-            </a>
-          </Card.Content>
-        </Card>
-      </div>
+      {error ? (
+        <h1>{error}</h1>
+      ) : (
+        <div className="card">
+          <Card>
+            <Image src={avatar} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>{name}</Card.Header>
+              <Card.Header>{userName}</Card.Header>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="user" />
+                {followers} Followers
+              </a>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="user" />
+                {repos} Repos
+              </a>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="user" />
+                {following} Following
+              </a>
+            </Card.Content>
+          </Card>
+        </div>
+      )}
     </div>
   );
 }
